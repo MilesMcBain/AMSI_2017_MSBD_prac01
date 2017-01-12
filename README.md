1dccd9738b10b9befd5839daf667bf77881f4147
MilesMcBain <miles.mcbain@gmail.com>
2017-01-12 01:40:16 +0000

    moving files into prac01 dir

---
+++ README.md
@@ -0,0 +1,2 @@
+# AMSI_summer_pracs
+Practicals for the Mathematics and Statistics of Big Data Course at AMSI Summer School 2017
---
+++ prac01/prerequisites.R
@@ -0,0 +1,4 @@
+install.packages(c("sparklyr", "tidyverse"))
+sparklyr::spark_install(version = "2.0.2")
+devtools::install_github("krlmlr/here")
+
